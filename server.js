c13c4febe8d0ef7149d4488a310def3cc8cdb783
Mustafa <mgords@gmail.com>
2016-01-08 16:01:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,9 +10,11 @@ app.engine('html', cons.swig);
 app.set('view engine', 'html');
 
 app.use(cookieParser());
-app.use(expressSession({secret:'MusSecret'}));
+app.use(expressSession({
+    secret: 'MusSecret'
+}));
 app.use(express.static(__dirname + '/public'));
-app.use("/assets",express.static(__dirname + "/assets"));
+app.use("/assets", express.static(__dirname + "/assets"));
 app.use(bodyParser.json());
 
 /*mongodb*/
@@ -21,97 +23,119 @@ var uristring = process.env.MONGOLAB_URI || 'mongodb://localhost/mean_stack';
 mongoose.connect(uristring);
 
 var userSchema = new mongoose.Schema({
-	username: { type: String, required: true, unique: true },
-	password: { type: String, required: true },
-	email: {type: String, required: true}
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    }
 });
 
 var User = mongoose.model('User', userSchema);
 /*end mongodb*/
 
-app.post('/send', function (req, res) {
-  console.log(req.body);
-  var sendBack = req.body.a + " server geri döndü.";
-  res.json(sendBack);
+app.post('/send', function(req, res) {
+    console.log(req.body);
+    var sendBack = req.body.a + " server geri döndü.";
+    res.json(sendBack);
 });
 
-app.post('/login',function(req,res){
-	delete req.session.user;
-	delete req.session.email;
-	var post = req.body;
-	User.findOne({ 'username': post.username }, function(err, user) {
-		if (err) throw err;
-		if(user && post.password == user.password){
-			req.session.user = user.username;
-			req.session.email = user.email;
-			console.log(req.session.user + " logged in.");
-			res.json({username: req.session.user});
-		}
-		else {
+app.post('/login', function(req, res) {
+    delete req.session.user;
+    delete req.session.email;
+    var post = req.body;
+    User.findOne({
+        'username': post.username
+    }, function(err, user) {
+        if (err) throw err;
+        if (user && post.password == user.password) {
+            req.session.user = user.username;
+            req.session.email = user.email;
+            console.log(req.session.user + " logged in.");
+            res.json({
+                username: req.session.user
+            });
+        } else {
             req.session.user = '';
-            res.json({username: req.session.user});
+            res.json({
+                username: req.session.user
+            });
         }
-	});
+    });
 });
 
-app.post('/signup', function (req, res) {
-	var post = req.body;
-	var new_user = new User({
-		username: post.username,
-		password: post.password,
-		email: post.email
-	});
-	new_user.save(function (err, new_user) {
-		if (err) {
+app.post('/signup', function(req, res) {
+    var post = req.body;
+    var new_user = new User({
+        username: post.username,
+        password: post.password,
+        email: post.email
+    });
+    new_user.save(function(err, new_user) {
+        if (err) {
             req.session.user = '';
-            res.json({username: req.session.user + 'dummy'});
-			console.log(post.username + " kullanici adi zaten kullaniliyor!");
-		}
-		else{
-			if(post.username) {
-                res.json({username: post.username});
-				console.log(post.username + ' basariyla eklendi!');
-			}
-			else{
+            res.json({
+                username: req.session.user + 'dummy'
+            });
+            console.log(post.username + " kullanici adi zaten kullaniliyor!");
+        } else {
+            if (post.username) {
+                res.json({
+                    username: post.username
+                });
+                console.log(post.username + ' basariyla eklendi!');
+            } else {
                 req.session.user = '';
-                res.json({username: req.session.user});
-    			console.log(post.username + " hata");
+                res.json({
+                    username: req.session.user
+                });
+                console.log(post.username + " hata");
             }
-		}
-	});
+        }
+    });
 });
 
 
-app.get('/getUserDetails', function (req, res) {
-	if(req.session.user === undefined){
-		req.session.user = '';
-	}
-	res.json({username: req.session.user});
+app.get('/getUserDetails', function(req, res) {
+    if (req.session.user === undefined) {
+        req.session.user = '';
+    }
+    res.json({
+        username: req.session.user
+    });
 });
 
-app.get('/getMainDetails', function (req, res) {
-	if(req.session.user != '') {
-		User.findOne({'username' : req.session.user}, function(err, user){
-			res.json({
-				username: user.username,
-				email: user.email,
-				password: user.password
-			});
-		});
+app.get('/getMainDetails', function(req, res) {
+    if (req.session.user != '') {
+        User.findOne({
+            'username': req.session.user
+        }, function(err, user) {
+            res.json({
+                username: user.username,
+                email: user.email,
+                password: user.password
+            });
+        });
 
-	}
-	else
-		res.json('');
+    } else
+        res.json('');
 });
 
-app.post('/logout', function(req,res){
+app.post('/logout', function(req, res) {
     console.log('logout');
     req.session.user = '';
     res.json(req.session.user);
 });
 
-app.all('*', function(req, res){
-	res.redirect('/');
+app.all('*', function(req, res) {
+    res.redirect('/');
 });
 
 var server = app.listen(port);
